59da538219155b6ad423df69483689cf9a261a45
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-10 13:41:32 +0300

    white.tsx file

---
+++ app/(modals)/white.tsx
@@ -0,0 +1,12 @@
+ import { View, Text } from 'react-native'
+
+ 
+ const white = () => {
+   return (
+     <View style={{ flex: 1, backgroundColor: '#fff'}}>
+       <Text>white</Text>
+     </View>
+   )
+ }
+ 
+ export default white
