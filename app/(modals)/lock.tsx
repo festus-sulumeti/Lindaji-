03818fbc4bca9a0b0f4198247c3cd51a51aa7935
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-10 13:44:11 +0300

    lock file

---
+++ app/(modals)/lock.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const lock = () => {
+  return (
+    <View>
+      <Text>lock</Text>
+    </View>
+  )
+}
+
+export default lock
