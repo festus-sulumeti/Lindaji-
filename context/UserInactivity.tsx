6ff1bcb305d98e4d06ffe2cae6f86f877503000e
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-10 14:04:36 +0300

    UserInactivityProvider with MMKV storage and AppState management

--- context/UserInactivity.tsx
+++ context/UserInactivity.tsx
@@ -1,4 +1,7 @@
+import { AppState } from 'react-native';
 import { MMKV } from 'react-native-mmkv';
+import { useRouter } from'expo-router'
+import { useRef } from 'react';
 
 const storage = new MMKV({
     id: 'userInactivity',
@@ -10,5 +13,7 @@ const LOCK_TIME = 3000;
 
 
 export const UserInactivityProvider = ( { children }: any ) => {
+    const appState = useRef(AppState.currentState);
+    const router = useRouter();
     return children;
 };
