6826ceac902f981c3d46594b02f62d74af4ca730
festus-sulumeti <sulumetifestus@outlook.com>
2024-09-10 13:57:51 +0300

    the updated layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,19 @@
 import { Stack } from 'expo-router';
+import { UserInactivityProvider } from '~/context/UserInactivity';
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <UserInactivityProvider>
+      <Stack>
+        <Stack.Screen name="(modals)/white" options={{
+          headerShown: false,
+          animation:'none',
+        }}/> 
+        <Stack.Screen name="(modals)/lock" options={{
+          headerShown: false,
+          animation:'none',
+        }}/>      
+      </Stack>
+    </UserInactivityProvider>
+  );
 }
